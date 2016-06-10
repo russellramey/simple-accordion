36940f4ef436ac3a9097f6e28b3d6c244df5f924
Russell <doublercreative@gmail.com>
2016-06-10 14:01:04 -0500

    improved JS function
    added self closing capability.

--- simple.accordion.js
+++ simple.accordion.js
@@ -1,16 +1,24 @@
 // Document Ready
 $( document ).ready(function() {
+    // Add function to container element
     $('.simple-accordion').simple_accordion();
 });
-// Simple Accordion
+// Simple Accordion funciton
 $.fn.simple_accordion = function(){
+    // Set your action element
     var action = $('.simple-accordion--action');
+    // Set you hidden content element
     var moredetails = $('.simple-accordion--content');
     $(action).click(function(){
-        $(action).removeClass('active');
-        $(moredetails).slideUp(150).removeClass('active');
-        $(this).parent().find(moredetails).slideDown(150).addClass('active');
-        $(this).addClass('active');
+        if (!$(this).hasClass('active')){
+            $(action).removeClass('active');
+            $(moredetails).slideUp(150).removeClass('active');
+            $(this).parent().find(moredetails).slideDown(150).addClass('active');
+            $(this).addClass('active');
+        } else {
+            $(action).removeClass('active');
+            $(moredetails).slideUp(150).removeClass('active');
+        }
         return false;
     });
 };
